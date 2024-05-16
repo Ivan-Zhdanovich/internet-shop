29c2792441f6cbd048cbaa27e6fdf1478f9c5895
Zhdanovich <Zhiv80@mail.ru>
2024-05-16 17:16:29 +0300

    init: creat internet shop app

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, vitae dicta repellendus nobis illo, omnis eveniet incidunt facilis recusandae consequuntur architecto nihil magni ratione, quo libero in ipsum expedita odit? Facilis asperiores quam tempora aspernatur pariatur quia nesciunt, deleniti dignissimos consequuntur similique, repellendus officiis rerum labore neque nobis optio? Consectetur.</div>
+    )
+}
